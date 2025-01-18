f72993665d041fa72dcdb26ece2125b1890363ba
Sourabh <sourabhmalame@gmail.com>
2025-01-18 10:46:21 +0530

    last

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,10 +7,58 @@ import Cars from "../../components/Cars/Cars";
 import Benefits from "../../components/Benefits/Benefits";
 import Questions from "../../components/Questions/Questions";
 import Contact from "../../components/Contact/Contact";
+import insta from "../../assets/instagram.png";
+import gmail from "../../assets/gmail.png";
+import whatsapp from "../../assets/whatapp.png"
+
 
 const Home = () => {
+
+    const openInstagram = () => {
+        window.open("https://www.instagram.com/tasmayi_motors_121/", "_blank");
+    };
+
+    const openWhatsApp = (number) => {
+        const whatsappURL = `https://wa.me/${number}`;
+        window.open(whatsappURL, "_blank");
+    };
+
+    const sendEmail = () => {
+        window.location.href = "mailto:tasmayimotors@gmail.com";
+    };
+
     return (
         <div className={styles.homeContainer}>
+            <div className={styles.sidelogo}>
+
+                {/* Instagram */}
+                <img
+                    className={styles.sideimg}
+                    src={insta}
+                    alt="Instagram"
+                    onClick={openInstagram}
+                    style={{ cursor: 'pointer' }}
+                />
+
+                {/* WhatsApp */}
+                <img
+                    className={styles.sideimg}
+                    src={whatsapp}
+                    alt="WhatsApp 1"
+                    onClick={() => openWhatsApp('+918855831697')}
+                    style={{ cursor: 'pointer' }}
+                />
+              
+
+                {/* Gmail */}
+                <img
+                    className={styles.sideimg}
+                    src={gmail}
+                    alt="Gmail"
+                    onClick={sendEmail}
+                    style={{ cursor: 'pointer' }}
+                />
+            </div>
             <HeroSection />
             <BuyFromUsSection />
             <Brands />
